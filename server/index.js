461d12814ba07a6c4b9d31cb32680449b85e6e1d
alexander shen <alexander.shen@gmail.com>
2023-09-22 22:02:03 -0400

    re-added general get route

--- server/index.js
+++ server/index.js
@@ -35,9 +35,9 @@ app.get('/', (req, res) => {
 })
 
 // All other GET requests not handled before will return our React app
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log("Testing database connection")
