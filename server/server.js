57874211b70ef6525477258f98fa9cc604537bfb
alexander shen <alexander.shen@gmail.com>
2023-09-22 21:04:44 -0400

    shutting down routes for testing

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ require("dotenv").config({ path: "./config.env" });
 const dbo = require("./db/conn.js");
 
 const port = process.env.PORT || 8080;
+
+console.log("Test log: managed to get to cors section")
+
 app.use(cors(
   {
         origin: ["*"],
@@ -15,17 +18,17 @@ app.use(cors(
 ));
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.use(require("./routes/ArtifactCards.js"));
-app.use(require("./routes/CharacterCards.js"));
-app.use(require("./routes/EventCards.js"));
-app.use(require("./routes/Statuses.js"));
-app.use(require("./routes/Summons.js"));
-app.use(require("./routes/SupportCards.js"));
-app.use(require("./routes/TalentCards.js"));
-app.use(require("./routes/WeaponCards.js"));
-app.use(require("./routes/FirebaseImageUrls.js"));
+// app.use(require("./routes/ArtifactCards.js"));
+// app.use(require("./routes/CharacterCards.js"));
+// app.use(require("./routes/EventCards.js"));
+// app.use(require("./routes/Statuses.js"));
+// app.use(require("./routes/Summons.js"));
+// app.use(require("./routes/SupportCards.js"));
+// app.use(require("./routes/TalentCards.js"));
+// app.use(require("./routes/WeaponCards.js"));
+// app.use(require("./routes/FirebaseImageUrls.js"));
 
 app.get('/', (req, res) => {
   res.json("Backend app is running")
@@ -37,10 +40,10 @@ app.get('/', (req, res) => {
 // });
 
 app.listen(port, () => {
+  console.log("Testing database connection")
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
-
   });
   console.log(`Server is running on port: ${port}`);
 });
