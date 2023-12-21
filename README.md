a1c2f15619b0361ebc4e2a801032f2dfb618956b
alexander shen <alexander.shen@gmail.com>
2023-12-20 22:51:42 -0500

    reverting changes

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 https://streamable.com/ciho7v
 
 ##### Description
-This is a deck builder website for the Genshin Impact TCG gamemode. You can build as many decks as you want, and download / import decks.
+This is a deck builder website for the Genshin Impact TCG gamemode. You can build as many decks as you want, and download / import decks. **This is current a work in progress, and changes might be made to the website.**
 
 Check it out at https://genshin-tcg-deck-builder.vercel.app!
--- server/index.js
+++ server/index.js
@@ -8,34 +8,12 @@ require("dotenv").config({ path: "./config.env" });
 const dbo = require("./db.js");
 const port = process.env.PORT || 8080;
 
-// test from tut
-const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "../client/build");
-
-app.use(express.static(buildPath)); // from tut
-
-app.use(express.json());
-
 app.use(
   cors({
     origin: "*",
   })
 );
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-app.get("/*", function(req, res){
-
-  res.sendFile(
-      path.join(__dirname, "../client/build/index.html"),
-      function (err) {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
-    );
-
-})
+app.use(express.json());
 
 app.use(require("./routes/ArtifactCards.js"));
 app.use(require("./routes/CharacterCards.js"));
